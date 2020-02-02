c3438c26ad08e0ed43f5362b4a3f6c6cac20d84a
Adrian Khashaki <Adrian.Khashaki@kolibrigames.com>
2020-02-02 13:09:31 +0100

    Nevermind forgot to stage

---
+++ Assets/Scripts/Honk.cs
@@ -0,0 +1,26 @@
+﻿using UniRx;
+using UnityEngine;
+
+[RequireComponent(typeof(CustomPlayerInput))]
+[RequireComponent(typeof(AudioSource))]
+public class Honk : MonoBehaviour
+{
+    public CustomPlayerInput CustomPlayerInput;
+    public AudioSource AudioSource;
+    
+    private void Start()
+    {
+        CustomPlayerInput.ButtonDown
+            .Where(down => down)
+            .Subscribe(_ => HonkHorn())
+            .AddTo(gameObject);
+    }
+
+    private void HonkHorn()
+    {
+        if (AudioSource.isPlaying)
+            return;
+
+        AudioSource.Play();
+    }
+}
