c25b83d4262014707b528bf0fcdb2c9fa090cf30
Adrian Khashaki <Adrian.Khashaki@kolibrigames.com>
2020-02-02 13:26:26 +0100

    Added Abyss

---
+++ Assets/Scripts/Abyss.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Abyss : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetPlayerAffinity() == 0)
+            return;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
